e8ca92696f01a26013c6f08b8c2ee01f33359290
xuyc <xmcy0011@sina.com>
2020-11-16 13:45:45 +0800

    added

--- robot_srv/main.go
+++ robot_srv/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/bitly/go-simplejson"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -25,9 +26,34 @@ func main() {
 			log.Println(err.Error())
 			return
 		}
+		root, err := simplejson.NewJson(body)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		nulValue := root.Get("nlu")
+		answerValue, err := nulValue.Get("answer").String()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		nulValue.Set("answer", fmt.Sprintf("%s 再加工----------", answerValue))
+
+		resData, err := nulValue.Encode()
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
 
 		log.Printf("http get/post,method:%s,header:%v,body:%s \n", request.Method, request.Header, string(body))
-		_, _ = fmt.Fprintf(writer, "success")
+
+		_, err = writer.Write(resData)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		//_, _ = fmt.Fprintf(writer, string(resData))
 	})
 	log.Print("server start on :", kListenPort)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", kListenPort), nil))
